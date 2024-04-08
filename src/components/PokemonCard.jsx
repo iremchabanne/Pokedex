22484aeb040216e9167a605dcf98d600e23b9ffe
iremchabanne <irem_kcakir@outlook.com>
2024-04-08 20:34:14 +0200

    Dynamic Pokemon Cards

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,21 @@
 function PokemonCard () {
+    const pokemon = pokemonList[1]
     return( <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+        {Object.hasOwn(pokemon, "imgSrc") ? <img src = {pokemon.imgSrc}/> : <p>???</p> }
+        <figcaption>{pokemon.name}</figcaption>
+
     </figure> )
 }
 
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
 export default PokemonCard
