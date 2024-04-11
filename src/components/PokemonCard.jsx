df6d22f222ed5ee69d87a0c76d58bd9833e71371
iremchabanne <irem_kcakir@outlook.com>
2024-04-11 11:32:47 +0200

    Added props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,12 @@
-function PokemonCard () {
-    const pokemon = pokemonList[1]
-    return( <figure>
-        {Object.hasOwn(pokemon, "imgSrc") ? <img src = {pokemon.imgSrc}/> : <p>???</p> }
-        <figcaption>{pokemon.name}</figcaption>
-
-    </figure> )
+function PokemonCard ({pokemon}) {
+    return( <figure className="card">
+    {pokemon.imgSrc ? (
+      <img className="card-img" src={pokemon.imgSrc} alt={pokemon.name} />
+    ) : (
+      <p>???</p>
+    )}
+    <figcaption>{pokemon.name}</figcaption>
+  </figure> )
 }
-
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
   
 export default PokemonCard
